918eb8a6ab98ad5b614870cd09ab489b9416ac1e
RonaldGC <ronald.gc3154@gmail.com>
2025-03-11 22:15:44 -0600

    Add initial project setup with ESLint, Tailwind CSS, and PostCSS configuration

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,6 +1,6 @@
-import { app, shell, BrowserWindow, ipcMain } from 'electron'
+import { electronApp, is, optimizer } from '@electron-toolkit/utils'
+import { app, BrowserWindow, ipcMain, shell } from 'electron'
 import { join } from 'path'
-import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
 
 function createWindow(): void {
@@ -11,9 +11,16 @@ function createWindow(): void {
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
+    center: true,
+    title: 'Edinomark',
+    frame: true,
+    visualEffectState: 'active',
+    titleBarStyle: 'default',
+    trafficLightPosition: { x: 15, y: 10 },
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
-      sandbox: false
+      sandbox: true,
+      contextIsolation: true
     }
   })
 
